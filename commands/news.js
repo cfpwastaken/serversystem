3a727e7610b594004c122f663c50592c198ff73f
Cfp <cfp@cfp.gotdns.ch>
2022-01-13 16:04:59 +0100

    News command

---
+++ commands/news.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "news",
+    description: "Send a news message",
+    category: "basic",
+    hide: false,
+    guildOnly: true,
+    options: [
+        {
+            type: 3,
+            name: "message",
+            description: "The message to send",
+            required: true
+        }
+    ],
+    run: async (bot, interaction, lang) => {
+        const embed = new Discord.MessageEmbed()
+            .setTitle(interaction.options.getString("title") || ":loudspeaker: News :newspaper:")
+            .setDescription(interaction.options.getString("message"))
+            .setColor("RANDOM")
+            .setFooter("News");
+        interaction.channel.send({ embeds: [embed] }).then(msg => {
+            if(msg.crosspostable) {
+                msg.crosspost();
+            }
+        });
+        interaction.reply({ content: lang.get("news_sent"), ephemeral: true });
+    }
+}
