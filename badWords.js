08fe08ef5961c396422c548be118f68c105d1b6a
Cfp <cfp@cfp.gotdns.ch>
2022-04-02 19:01:23 +0200

    Even more

--- badWords.js
+++ badWords.js
@@ -1,9 +1,16 @@
 const badWords = ["fuck", "shit", "piss off", "dick", "asshole", "bitch", "bastard", "wixxer", "wichs", "noob", "nub", "ratio", "didn't ask", "stay mad", "cry about it", "negative iq", "your opinion is wrong", "get a life", "touch grass", "n00b"]
+const bypassWords = {
+  "¢": "c",
+}
 
 module.exports = function check(content) {
   let containsBadWord = false;
   // remove invisible characters and diacritics from the string as they can be used to bypass the bad word check
   let censored = content.normalize("NFD").replace(/[^\u0000-\u007E]/g, "").replace(/[\u0300-\u036f]/g, '');
+  // remove bypass words
+  for (let word in bypassWords) {
+    censored = censored.replace(word, bypassWords[word]);
+  }
   for(const badWord in badWords) {
       if(censored.toLowerCase().match(new RegExp(badWords[badWord], "g"))) {
           containsBadWord = true;
